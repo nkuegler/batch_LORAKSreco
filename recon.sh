b402dd5e808b5b4728d3111b7b05aeadfe5f773a
Niklas Kuegler <kuegler@cbs.mpg.de>
2026-09-16 12:33:51 +0200

    Move everything from ironsleep_TH subdirectory to repository root

--- recon.sh
+++ recon.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 16					# 16 cores for fast reco
 #SBATCH --mem 500G				# 0.5 mm fully sampled is around 220G, need maybe double
 #SBATCH --time 1440				# 10 echoes at 2 hours per echo but some nodes take 3 hours per echo
-#SBATCH -o /data/u_kuegler_software/git/loraks_reconstruction/logs/ironsleep_TH/%j.out	# redirect the output
+#SBATCH -o /data/u_kuegler_software/git/batch_LORAKSreco/logs/ironsleep_TH/%j.out	# redirect the output
 #
 # Real values for (16 cores, 500G request) are 213G RAM 8.5hrs time 27G file (PDw/T1w), 121G 6.5hrs 10G (MTw)
 
@@ -33,7 +33,10 @@ fi
 start=$(date +%s)
 
 MATLAB -v 24.2 matlab -batch "reconstruction('$rawdata','$outdir','$config');exit" -sd /data/u_kuegler_software/git/image-reconstruction
+matlab_status=$?
 
 end=$(date +%s)
 duration=$((end - start))
 echo "Duration: $((duration / 60)) minutes"
+
+exit "$matlab_status"
